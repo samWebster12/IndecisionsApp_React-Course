9254c0d4debe23fe048b2e2960f68c2a9c3677b9
samWebster12 <samuelalexwebster@gmail.com>
2020-09-16 19:58:53 -0400

    babel downloaded

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
+'use strict';
+
 console.log('App is running');
+
+//JSX - JavaScript XML
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecisions App'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log('App is running');
+
+//JSX - JavaScript XML
+var template = <h1>Indecisions App</h1>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
